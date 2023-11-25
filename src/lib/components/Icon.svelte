1886fe6a63a5dc55a5de4de6b4e485efd50f5906
pebloop <pebloop@outlook.com>
2023-11-25 22:27:34 +0100

    Add icons'

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,5 +1,80 @@
 <script lang="ts">
 
+    export let icon: string;
+    export let color: string = '';
+    export let fill: boolean = false;
+    export let size: string = 'larger';
+    export let weight: number = 100;
+    export let grade: number = 0;
+    export let opticalSize: number = 24;
+    export let style: string = '';
+
+    $: fillValue = fill ? 1 : 0;
+
+    function getColorStyle(color: string) {
+        if (color) {
+            return "color";
+        } else {
+            return "default-color";
+        }
+    }
+
+    function getStyle(style: string) {
+        switch (style) {
+            case 'outlined':
+                return 'material-symbols-outlined';
+            case 'rounded':
+                return 'material-symbols-rounded';
+            case 'sharp':
+                return 'material-symbols-sharp';
+            default:
+                return 'material-symbols-outlined';
+        }
+    }
+
+    $: styleClass = getStyle(style);
+    $: colorClass = getColorStyle(color);
 </script>
 
-<span class="material-symbols-outlined">search</span>
+<span class="{styleClass} {colorClass}"
+      style="--color: {color}; --size: {size}; --weight: {weight}; --fill: {fillValue}; --grade: {grade}; --opticalSize: {opticalSize};">
+    {icon}
+</span>
+
+<style lang="postcss">
+
+    .material-symbols-outlined {
+        font-size: var(--size);
+        font-variation-settings:
+                'FILL' var(--fill),
+                'wght' var(--weight),
+                'GRAD' var(--grade),
+                'opsz' var(--opticalSize);
+    }
+
+    .material-symbols-rounded {
+        font-size: var(--size);
+        font-variation-settings:
+                'FILL' var(--fill),
+                'wght' var(--weight),
+                'GRAD' var(--grade),
+                'opsz' var(--opticalSize);
+    }
+
+    .material-symbols-sharp {
+        font-size: var(--size);
+        font-variation-settings:
+                'FILL' var(--fill),
+                'wght' var(--weight),
+                'GRAD' var(--grade),
+                'opsz' var(--opticalSize);
+    }
+
+    .default-color {
+        @apply text-darkMode dark:text-lightmode;
+    }
+
+    .color {
+        color: var(--color);
+    }
+</style>
--- src/stories/Icon.stories.ts
+++ src/stories/Icon.stories.ts
@@ -6,12 +6,50 @@ const meta = {
   title: 'Icon',
   component: Icon,
   tags: ['autodocs'],
+  args: {
+    icon: "home",
+    size: "100px",
+  }
 } satisfies Meta<Icon>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Outlined: Story = {
+};
+
+export const Sharp: Story = {
   args: {
+    style: "sharp"
   },
+};
+
+export const Rounded: Story = {
+  args: {
+    style: "rounded"
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    fill: true
+  },
+};
+
+export const Colored: Story = {
+  args: {
+    color: "red"
+  },
+};
+
+export const Size: Story = {
+  args: {
+    size: "50px"
+  },
+};
+
+export const weight: Story = {
+    args: {
+        weight: "bold"
+    },
 };
